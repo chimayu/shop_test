763971dfae1a3548ededef43041a5279bdc38489
mayumi <chimayu@gmail.com>
2020-01-13 15:11:01 +0900

    スタッフ追加ボタン実装

--- staff/staff_branch.php
+++ staff/staff_branch.php
@@ -2,6 +2,11 @@
 
 $staff_code = $_POST['staffcode'];
 
+if(isset($_POST['add']) == true){
+    header('Location:staff_add.php');
+    exit();
+}
+
 if(isset($_POST['edit']) == true){
     if(isset($_POST['staffcode']) == false){
         header('Location:staff_ng.php');
