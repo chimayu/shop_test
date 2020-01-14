0553a1bc24d2b586b4bd104c433693eb5fa05a1d
mayumi <chimayu@gmail.com>
2020-01-14 14:07:06 +0900

    assetsフォルダ作成

--- staff/staff_add_done.php
+++ staff/staff_add_done.php
@@ -34,7 +34,7 @@ try{
     print 'さんを追加しました。<br />';
 
 } catch (Exception $e) {
-    print 'ただいま障害により大変ご迷惑をお掛けしております。';
+    print '<div style="text-align:center;"><br /><br /><img src="assets/img/img01.jpg" style="width:400px;"><br />ただいま障害により大変ご迷惑をお掛けしております。</div>';
     exit();
 }
 
